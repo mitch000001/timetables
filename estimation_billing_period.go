6a1c97a55e1d1fc96a80333f74433af766aa3cee
Michael Wagner <mitch.wagna@gmail.com>
2015-09-02 11:47:01 +0200

    Remove cumulated values from estimation billing period

--- estimation_billing_period.go
+++ estimation_billing_period.go
@@ -3,15 +3,11 @@ package timetables
 import "github.com/mitch000001/go-harvest/harvest"
 
 type PlanConfig struct {
-	Year                          int
-	BusinessDays                  float64
-	VacationInterestDays          float64
-	SicknessInterestDays          float64
-	ChildCareInterestDays         float64
-	CumulatedBusinessDays         float64
-	CumulatedVacationInterestDays float64
-	CumulatedSicknessInterestDays float64
-	CumulatedBilledDays           float64
+	Year                  int
+	BusinessDays          float64
+	VacationInterestDays  float64
+	SicknessInterestDays  float64
+	ChildCareInterestDays float64
 }
 
 type UserConfig struct {
@@ -28,14 +24,11 @@ func CreateEstimationBillingPeriod(period Period, planConfig PlanConfig, userCon
 		Timeframe:                     period.Timeframe,
 		UserID:                        "1",
 		BusinessDays:                  NewFloat(period.BusinessDays),
-		CumulatedBusinessDays:         NewFloat(period.BusinessDays).Add(NewFloat(planConfig.CumulatedBusinessDays)),
 		RemainingVacationInterestDays: NewFloat(userConfig.remainingVacationInterestDays),
 	}
 	shareOfYear := NewFloat(period.BusinessDays).Div(NewFloat(planConfig.BusinessDays))
 
 	estimationPeriod.VacationInterestDays = NewFloat(userConfig.workingDegree).Mul(NewFloat(planConfig.VacationInterestDays)).Mul(shareOfYear)
-
-	estimationPeriod.CumulatedVacationInterestDays = estimationPeriod.VacationInterestDays.Add(NewFloat(planConfig.CumulatedVacationInterestDays))
 
 	sicknessInterest := NewFloat(planConfig.SicknessInterestDays)
 	if userConfig.hasChild {
@@ -44,33 +37,22 @@ func CreateEstimationBillingPeriod(period Period, planConfig PlanConfig, userCon
 	sicknessInterestShare := sicknessInterest.Mul(shareOfYear)
 	estimationPeriod.SicknessInterestDays = sicknessInterestShare.Mul(NewFloat(userConfig.workingDegree))
 
-	estimationPeriod.CumulatedSicknessInterestDays = estimationPeriod.SicknessInterestDays.Add(NewFloat(planConfig.CumulatedSicknessInterestDays))
-
 	unbilled := estimationPeriod.SicknessInterestDays.Add(estimationPeriod.VacationInterestDays).Add(estimationPeriod.RemainingVacationInterestDays)
 	estimationPeriod.BilledDays = NewFloat(period.BusinessDays).Sub(unbilled).Mul(NewFloat(userConfig.billingDegree))
 
-	estimationPeriod.CumulatedBilledDays = estimationPeriod.BilledDays.Add(NewFloat(planConfig.CumulatedBilledDays))
-
 	estimationPeriod.EffectiveBillingDegree = estimationPeriod.BilledDays.Div(NewFloat(period.BusinessDays))
-
-	estimationPeriod.CumulatedEffectiveBillingDegree = estimationPeriod.CumulatedBilledDays.Div(estimationPeriod.CumulatedBusinessDays)
 
 	return estimationPeriod, nil
 }
 
 type EstimationBillingPeriod struct {
-	ID                              string
-	Timeframe                       harvest.Timeframe
-	UserID                          string
-	BusinessDays                    *Float
-	CumulatedBusinessDays           *Float
-	VacationInterestDays            *Float
-	CumulatedVacationInterestDays   *Float
-	RemainingVacationInterestDays   *Float
-	SicknessInterestDays            *Float
-	CumulatedSicknessInterestDays   *Float
-	BilledDays                      *Float
-	CumulatedBilledDays             *Float
-	EffectiveBillingDegree          *Float
-	CumulatedEffectiveBillingDegree *Float
+	ID                            string
+	Timeframe                     harvest.Timeframe
+	UserID                        string
+	BusinessDays                  *Float
+	VacationInterestDays          *Float
+	RemainingVacationInterestDays *Float
+	SicknessInterestDays          *Float
+	BilledDays                    *Float
+	EffectiveBillingDegree        *Float
 }
